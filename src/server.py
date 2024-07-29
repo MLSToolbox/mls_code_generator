69f7a003e5a3c03d069f2827726e4aae7c6c0769
MikoMikarro <lopezcuestam@gmail.com>
2024-07-29 11:34:16 +0200

    Add

--- src/server.py
+++ src/server.py
@@ -55,9 +55,9 @@ def home():
 @app.route('/api/get_config', methods=['GET', 'POST'])
 @cross_origin()
 def get_config():
-    node_config_path = './config/nodes.json'
-    options_config_path = './config/options.json'
-    socket_config_path = './config/sockets.json'
+    node_config_path = './mls_code_generator_config/nodes.json'
+    options_config_path = './mls_code_generator_config/options.json'
+    socket_config_path = './mls_code_generator_config/sockets.json'
 
     with open(node_config_path, 'r') as file:
         node_config = json.load(file)
