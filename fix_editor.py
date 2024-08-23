ffece5b3fbf7dc646fc709b77440ebc289768147
MikoMikarro <lopezcuestam@gmail.com>
2024-08-23 08:46:11 +0200

    linting

--- fix_editor.py
+++ fix_editor.py
@@ -1,14 +1,17 @@
+""" Fix editor.json file """
+
 import json
 from src.mls_code_generator.utils import fix_editor
 
 if __name__ == '__main__':
 
-    file_path = 'editor.json'
-    out_path = 'fixed_editor.json'
+    FILE_PATH = 'editor.json'
+    OUT_PATH = 'fixed_editor.json'
 
-    content = json.load(open(file_path))
+    with open(FILE_PATH, 'r', encoding='utf-8') as file:
+        content = json.load(file)
 
-    new_content = fix_editor(content)
+        new_content = fix_editor(content)
 
-    json.dump(new_content, open(out_path, 'w'), indent=4)
-
+        with open(OUT_PATH, 'w', encoding='utf-8') as out_file:
+            json.dump(new_content, out_file, indent=4)
